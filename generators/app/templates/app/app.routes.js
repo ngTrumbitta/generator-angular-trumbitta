aca3856b3ec1ee88e1e23d09d80cf4147d063708
Andrea D <nkjoep@gmail.com>
2015-11-18 10:29:36 +0100

    Update app.routes.js

--- generators/app/templates/app/app.routes.js
+++ generators/app/templates/app/app.routes.js
@@ -1,3 +1,20 @@
+/**
+  * @ngdoc object
+  * @name app.routes
+  * @module app
+  * @requires app.controllers.home
+  * @requires app.controllers.tube
+  * @requires app.controllers.supplierList
+  * @requires app.controllers.supplier
+  *
+  * @description
+  * This module configure the `ui-router` with the states.
+  *
+  * The configured states are:
+  * - home
+  *
+**/
+
 angular
   .module('app.routes', [
     'ui.router',
